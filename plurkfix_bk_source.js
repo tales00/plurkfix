48338d6423c4307ef78eea3f806982b92e4f9b12
tales00 <tales00@users.noreply.github.com>
2015-05-01 19:24:56 +0800

    Create plurkfix_bk_source.js
    書籤程式部份的程式碼

---
+++ plurkfix_bk_source.js
@@ -0,0 +1,19 @@
+// 將註解次行的程式碼存成書籤然後噗文的頁面啟動。
+// 這只是個啟動器，
+// 會動態載入真正的程式碼（ plurkfix.js ），載入的位置記得要修改
+javascript:var%20e=document.createElement(&quot;script&quot;);e.setAttribute(&quot;language&quot;,&quot;javascript&quot;);e.setAttribute(&quot;charset&quot;,&quot;utf-8&quot;);e.setAttribute(&quot;src&quot;,&quot;https://googledrive.com/host/0B80HrwML0t-ja1I2YVpiUF9NM2c&quot;);document.body.appendChild(e);void(0);
+
+
+/**
+ * 
+ * 原始格式在這裡
+ * 要載入的 JS 碼放在 src 後面的網址裡頭
+ * 
+var e=document.createElement("script");
+e.setAttribute("language","javascript");
+e.setAttribute("charset","utf-8");
+e.setAttribute("src","https://googledrive.com/host/0B80HrwML0t-ja1I2YVpiUF9NM2c");
+document.body.appendChild(e);
+void(0);
+
+*/
